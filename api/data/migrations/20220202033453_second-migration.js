11a08deebf030dc543c94a0911e9d7c1172efe2d
MasterHap <76802578+MasterHap@users.noreply.github.com>
2022-02-01 22:43:09 -0500

    second migration wip

--- api/data/migrations/20220202033453_second-migration.js
+++ api/data/migrations/20220202033453_second-migration.js
@@ -1,5 +1,9 @@
 exports.up = function(knex) {
   return knex.schema
+    .createTable('class', classes => {
+        classes.increments('class_id')
+        classes.string('class_name', 52).notNullable().unique()
+    })
 };
 
 exports.down = function(knex) {
