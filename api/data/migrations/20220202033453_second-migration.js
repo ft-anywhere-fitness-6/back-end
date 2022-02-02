47a47e85047dfe90939a622689e3556f72f152ad
MasterHap <76802578+MasterHap@users.noreply.github.com>
2022-02-01 22:49:55 -0500

    wip

--- api/data/migrations/20220202033453_second-migration.js
+++ api/data/migrations/20220202033453_second-migration.js
@@ -3,6 +3,10 @@ exports.up = function(knex) {
     .createTable('class', classes => {
         classes.increments('class_id')
         classes.string('class_name', 52).notNullable().unique()
+        classes.string('class_type',32).notNullable()
+        classes.time('start_time')
+        classes.string('class_duration').notNullable()
+        classes.date('class_date')
     })
 };
 
