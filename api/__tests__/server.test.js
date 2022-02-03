dee1a36407a8e3e8793d748e381d7e13078090c6
MasterHap <76802578+MasterHap@users.noreply.github.com>
2022-02-02 23:48:41 -0500

    testing endpoints

--- api/__tests__/server.test.js
+++ api/__tests__/server.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest')
 const server = require('../server')
 const db = require('../data/db-config')
+const bcrypt =require('bcrypt')
 
 beforeAll(async () => {
   await db.migrate.rollback()
@@ -20,5 +21,25 @@ it('sanity check', () => {
 describe('server.js', () => {
   it('is the correct testing environment', async () => {
     expect(process.env.NODE_ENV).toBe('testing')
+  })
+})
+describe('Get /users', () => {
+  test('returns a status 200 Created', async () => {
+    const res = await request(server).get('/api/users');
+    expect(res.status).toBe(200)
+  })
+})
+
+describe('POST /register', () => {
+  test('responds with proper status on success', async () => {
+    const res = await request(server).post('/auth/register').send({ username: 'jake', password: '1234' })
+    expect(res.status).toBe(404)
+  })
+  
+  describe('Get /:user_id/attending', () => {
+    test('gets all attending classes', async () => {
+      const res = await request(server).get('/:user_id/attending');
+      expect(res.status).toBe(404)
+    })
   })
 })
